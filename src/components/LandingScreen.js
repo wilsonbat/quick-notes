22316ab076d9bbf806e15afa28e6d5dc91a06a3e
vivek.velu <vivek.velu@asurion.com>
2020-02-15 06:46:29 -0600

    adding landing screen -4

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Keyboard, StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+ 
+import Button from "./components/Button";
 
-export default class SettingsScreen extends React.Component {
+export default class LandingScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.header}>Settings</Text>
-        </View>
+        <View style={styles.container}>
+        <ScrollView style={{ flex: 1, flexDirection: 'column' }}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.textInput}
+              //placeholder={i18n.t('settings.name_placeholder')}
+              maxLength={20}
+              onBlur={Keyboard.dismiss}
+              value={this.state.name}
+              onChangeText={this.handleNameChange}
+            />
+          </View>
+          <View style={[styles.inputContainer, { flex: 1, justifyContent: 'flex-end' }]}>
+            <Button onPress={this.handleSubmit}>
+             
+            </Button>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 45,
-    backgroundColor: '#F5FCFF',
-  },
-  header: {
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 10,
-    fontWeight: 'bold'
-  }
-});
+    container: {
+      flex: 1,
+      paddingTop: 45
+    },
+    inputContainer: {
+      paddingTop: 15
+    },
+    textInput: {
+      borderColor: '#CCCCCC',
+      borderTopWidth: 1,
+      borderBottomWidth: 1,
+      height: 50,
+      fontSize: 25,
+      paddingLeft: 20,
+      paddingRight: 20
+    }
+  });
